e57816bffe7f85a550c46e371e1a55028222ac9c
absormu <absoralvord07@gmail.com>
2023-07-11 22:25:47 +0700

    create vote

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,11 +3,12 @@ interface Props {
     text: string;
     type?: "primary" | "secondary";
     className?:string;
+    onClick?: ()=>void;
 }
 
 export default function Button(props:Props) {
   return (
-    <button className={`bg-black border-2 border-black text-white px-3 py-2 hover:bg-zinc-800
+    <button onClick={props.onClick} className={`bg-black border-2 border-black text-white px-3 py-2 hover:bg-zinc-800
         ${props.type === "secondary" && "bg-white border-2 border-black text-black px-3 py-2 hover:text-white hover:bg-black"}
         ${props.className}
     `}>
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Button from "./Button";
 
 
 export default function Menu() {
+  const router = useRouter()
+
   return  (
     <div className="flex justify-between py-5">
-     <Image src={"/assets/Jujurly.svg"} alt="Jujurly" height={100} width={100}/>   
+     <Image src={"/assets/Jujurly.svg"} alt="Jujurly" height={100} width={100} onClick={() => router.push("/")} className={"cursor-pointer"}/>   
      <Button text="Login"/>
     </div>
   );
